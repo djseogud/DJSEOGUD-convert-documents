600eafe1282bab0d3576d1c7f3ee733531f57430
kanghyun <kanghyun@ad.unc.edu>
2017-07-17 23:28:03 -0400

    added all of contents

---
+++ DJSEOGUD-convert-documents.sh
@@ -0,0 +1,15 @@
+# Declare input argument as a variable
+INPUTFILE=$1
+OUTPUTNAME=$INPUTFILE
+
+# Convert markdown to HTML
+pandoc -o $OUTPUTNAME.html $INPUTFILE 
+# Convert markdown to DOCX
+pandoc -o $OUTPUTNAME.docx $INPUTFILE
+# Convert markdown to ODT
+pandoc -o $OUTPUTNAME.odt $INPUTFILE
+# Convert markdown to PDF
+pandoc -o $OUTPUTNAME.pdf --latex-engine=pdflatex $INPUTFILE
+
+# Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Converted $INPUTFILE to HTML, DOCX, ODT, PDF, and "
